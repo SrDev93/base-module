56c08eeda2e8172ce11ed94320f6ac165aa7bd71
Sajad Rahmani <38728381+SrDev93@users.noreply.github.com>
2023-07-04 12:02:06 +0330

    update

--- Routes/web.php
+++ Routes/web.php
@@ -13,7 +13,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function() {
+Route::prefix('panel')->group(function() {
     Route::resource('setting', 'BaseController');
     Route::resource('newsletter', 'NewsletterController');
     Route::resource('contact', 'ContactController');
