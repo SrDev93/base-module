9b9746f0263350fb05322cde5395071e97525729
Sajad Rahmani <38728381+SrDev93@users.noreply.github.com>
2024-10-29 10:45:15 +0330

    Update Comment.php

--- Entities/Comment.php
+++ Entities/Comment.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Base\Entities;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -14,6 +15,11 @@ class Comment extends Model
     protected static function newFactory()
     {
         return \Modules\Base\Database\factories\CommentFactory::new();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
     public function parent()
